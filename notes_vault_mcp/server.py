5c51c428cb1baf1e5c431896a1e3ea053bea0ed1
Carl Green <carl.green@gronare.se>
2026-09-11 10:56:09 +0200

    0.4.3

--- notes_vault_mcp/server.py
+++ notes_vault_mcp/server.py
@@ -15,7 +15,7 @@ from notes_vault_mcp.schema import instructions
 from notes_vault_mcp.search import render, search
 from notes_vault_mcp.vault import SingleVault, Vault, VaultResolver, open_vault
 
-VERSION = "0.4.2"
+VERSION = "0.4.3"
 
 EXPECTED_FAILURES = (VaultError, FrontmatterError, notes.ValidationError, ScopeError)
 
